513d9117ea93be6f92617a95216d89becf9458db
Kayode <ogkayode1@gmail.com>
2024-11-20 00:42:07 +0100

    % of bachelors degree

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -8,9 +8,16 @@ data = pd.read_csv(file_path, skipinitialspace=True)
 
 print(data.head()) # checking the structure of the dataset
 
-print(data.columns)   # confirm the column names
+print(data.columns)   # confirming the column names
 
 # calculate the number of people per race
 race_count = data["Race"].value_counts()
 print(race_count)
 
+# calculate the average age of men
+average_age_men = data[data["Gender"] == "Male"]["Age"].mean()
+print(average_age_men)
+
+# calculate percentage of people with Bachelors Degree
+percentage_of_bachelors = (data["Education"] == "Bachelors").mean() * 100
+print(percentage_of_bachelors)
