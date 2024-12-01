6bf836a15a18d59bfd169f2960258a59c4697729
kratoshi <ogkayode1@gmail.com>
2024-12-01 22:20:24 +0100

    new commit

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -23,3 +23,14 @@ print(average_age_men)
 # calculate percentage of people with Bachelors Degree
 percentage_of_bachelors = (data["Education"] == "Bachelors").mean() * 100
 print(percentage_of_bachelors)
+
+# percentage of people with advanced education (Bachelors, Masters, or Doctorate) that make more than 50K
+advanced_degree = ["Bachelors", "Masters", "Doctorate"]
+advanced_educated = data[data["Education"].isin(advanced_degree)]
+percentage_advanced_degree = (advanced_educated["Salary"] == ">50K").mean() * 100
+print(percentage_advanced_degree)
+
+# percentage of people without advanced education that earn more than 50K
+nonadvanced_educated = data[~data["Education"].isin(advanced_degree)]
+percentage_nonadvanced_educated = (nonadvanced_educated["Salary"] == ">50K").mean() * 100
+print(percentage_nonadvanced_educated)
